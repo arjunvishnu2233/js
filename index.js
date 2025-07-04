edab1c4f04c9c37b70712375d6d3f9d251febb59
arjunvishnuzz2233 <arjunvishnuzz00@gmail.com>
2025-07-04 16:01:00 +0530

    the file is added

---
+++ index.js
@@ -0,0 +1,155 @@
+//   {
+//     let a=5;
+//     let b=6;
+//       console.log(a+b);
+//       console.log(a-b);
+//       console.log(a*b);
+//       console.log(a/b);
+//       console.log(a%b);
+
+// let c=5;
+// c++;
+// console.log(c);
+// let d=13;
+// d%=6;
+// console.log(d);
+
+//      let e=5;
+//      let f=6;
+//      let fg=(e<=f);  
+//      console.log(fg);
+     
+//   }
+//   let a=10;
+//   let b=20;
+//   let c=40;
+//   d=b>a;
+//   console.log(d);
+//   e=c<a;
+//   console.log(e);
+//   f=d||e;
+//  console.log(f);
+
+//  let i=10;
+//  let j=20;
+
+//   if(a>b)
+//   {
+// console.log("i is greater");
+//   }
+//   else{
+//     console.log("j is greater");
+    
+//   }
+//  let day =5;
+//   if (day==1){
+//   console.log("sunday");
+//   }
+//   else if(day==2){
+//     console.log("monday");
+//   }
+//   else if(day==3){
+//     console.log("tuesday");
+//   }
+// else if(day==4){
+//   console.log("wednesday");
+// }else if(day==5){
+//   console.log("thursday");
+// }else if(day==6){
+//   console.log("friday");
+// }else if(day==7){
+//   console.log("saturday");
+// }
+// else{
+//   console.log("not a valid number");
+  
+// }
+// switch(day)
+// {
+// case 1: console.log("sunday");
+// break;
+// case 2: console.log("monday");
+// break;
+// case 3: console.log("tuesday");
+// break;
+// case 4: console.log("wednesday");
+// break;
+// case 5: console.log("thursday");
+// break;
+// case 6: console.log("friday");
+// break;
+// case 7: console.log("saturday");
+// break;
+// default : console.log("not a valid date");
+// break;
+
+// let k=1;
+// let l=3;
+// value= 5;
+// if (value==1)
+// {
+//   console.log("sum"+(k+l));
+// }
+//  else if(value==2)
+//     {
+//     console.log("sub"+(k-l));
+//     }
+//    else if(value==3)
+//       {
+//       console.log("product"+(k*l));
+//       }
+//    else  if(value==4)
+//       {
+//       console.log("divide"+(k/l));
+//       }
+//     else {
+//       console.log("not a valid ");
+      
+//     } 
+
+    let n=100;
+    let i=1;
+     
+    do
+    {
+      if(i%2==1){
+        console.log(i);
+      }
+      i++;
+    }
+      while(i<=100)
+      
+
+let m=100;
+let o=0;
+
+while(o<=100)
+{
+  if(o%3==0&&o%5==0){
+    console.log("@");
+  }
+  else if(o%5==0){
+    console.log("*");
+  }
+   else if(o%3==0)
+   { 
+    console.log("&"); 
+  }
+  else{
+    console.log(o);
+    
+  }
+o++;
+}
+  
+
+
+
+
+
+
+  
+
+   
+
+  
